0928515abaa38a162397ea4bfac1506d6568aec6
MAYANK RAJ <123razz321@gmail.com>
2026-05-03 03:36:00 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',      // <== This creates the 'out' folder
+  images: {
+    unoptimized: true,   // <== Required for static sites
+  },
 };
 
 export default nextConfig;
